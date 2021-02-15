81b59c43d5dd78391b3aae665f603942cdd7a3b9
Milos Prokic <milos@iseusa.org>
2021-02-15 16:34:51 -0500

    removed components that are not used and the ones that will not be used in short-med term. modified db init script

--- db/init.sql
+++ db/init.sql
@@ -1,32 +1,35 @@
-CREATE TABLE IF NOT EXISTS stock (
+CREATE TABLE IF NOT EXISTS asset (
     id SERIAL PRIMARY KEY, 
     symbol TEXT NOT NULL UNIQUE, 
     name TEXT NOT NULL,
     exchange TEXT NOT NULL,
-    shortable BOOLEAN NOT NULL
+    shortable BOOLEAN NOT NULL,
+    class ENUM('stock', 'crypto', 'forex'),
+    source ENUM('alpaca', 'polygon', 'binance'),
+    source_id TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS ohlc_data 
+CREATE TABLE IF NOT EXISTS asset_price 
 (
   id SERIAL PRIMARY KEY,
   dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  stock_id INTEGER NOT NULL,
+  asset_id INTEGER NOT NULL,
   open NUMERIC NOT NULL,
   high NUMERIC NOT NULL,
   low NUMERIC NOT NULL,
   close NUMERIC NOT NULL,
   volume NUMERIC NOT NULL,
-  CONSTRAINT fk_stock FOREIGN KEY(stock_id) REFERENCES stock(id)
+  CONSTRAINT fk_asset FOREIGN KEY(asset_id) REFERENCES asset(id)
 );
 
-CREATE TABLE IF NOT EXISTS tick_data  
+CREATE TABLE IF NOT EXISTS asset_tick  
 (
   id SERIAL PRIMARY KEY,
   dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  stock_id INTEGER NOT NULL,
+  asset_id INTEGER NOT NULL,
   bid NUMERIC NOT NULL,
   ask NUMERIC NOT NULL,
   bid_vol NUMERIC,
   ask_vol NUMERIC,
-  CONSTRAINT fk_stock FOREIGN KEY(stock_id) REFERENCES stock(id)
+  CONSTRAINT fk_asset FOREIGN KEY(asset_id) REFERENCES asset(id)
 );
